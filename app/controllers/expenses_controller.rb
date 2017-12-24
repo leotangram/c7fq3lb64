b3b115cfec4ad0b502cab3f75ba7c73df85af1a1
leotangram <leotangram@gmail.com>
2017-12-24 16:36:59 -0500

    3

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,8 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
   def index
-    user = User.find(current_user_id)
+    user = User.find(current_user.id)
      @expenses = user.expenses.order("date DESC")
-     @expenses = @expenses.where("concept LIKE :concept", concept: "%#{params[:concept]}%")
-     @expenses = @expenses.where("category_id LIKE :category_id", category_id: "%#{params[:category_id]}%")
+     @expenses = Expense.where(user_id: current_user.id).order("date DESC")
   end
 end
